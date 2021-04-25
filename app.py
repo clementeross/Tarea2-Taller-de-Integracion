088bb4e0fb9a02f11006f0a5b262f3a9a6fca24f
clementeross <cross3@uc.cl>
2021-04-24 22:02:26 -0400

    forgot

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import json
 
 
 # APP
-# API_URl = 'https://tarea2-cross3.herokuapp.com'
-API_URL = ''
+API_URl = 'https://tarea2-cross3.herokuapp.com'
+# API_URL = ''
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI']='postgresql://fhvzicwnvvpgoe:7c0f9dedd7dbc78f6bf63e7be2a0213cf70dabdd49c53af645d7dd879f4156ec@ec2-54-90-211-192.compute-1.amazonaws.com:5432/d734kvsp6ihc0e'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:ross1963@localhost/tarea2'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://fhvzicwnvvpgoe:7c0f9dedd7dbc78f6bf63e7be2a0213cf70dabdd49c53af645d7dd879f4156ec@ec2-54-90-211-192.compute-1.amazonaws.com:5432/d734kvsp6ihc0e'
+# app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:ross1963@localhost/tarea2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 # TABLES
